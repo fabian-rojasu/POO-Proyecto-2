a3224f3db9a51bf6af4ebd312e58df9fa2900f52
jeisonbcr7 <113469434+jeisonbcr7@users.noreply.github.com>
2022-11-16 22:35:07 -0600

    documentacion de comparadores

--- Comparadores/Comparador.java
+++ Comparadores/Comparador.java
@@ -2,7 +2,9 @@ package Comparadores;
 
 import java.time.LocalDateTime;
 
-
+/*
+ *Clase implementada de Comparable, para comparar 2 puntajes
+ */
 public class Comparador implements Comparable<Comparador> {
     private int puntaje;
     private LocalDateTime fecha;
@@ -12,7 +14,7 @@ public class Comparador implements Comparable<Comparador> {
         this.fecha = f;
     }
 
-
+// metodo para comparar los 2 puntajes, retorna numeros negativos si es menor, 0 si son iguales o numeros naturales si es mayor
     @Override
     public int compareTo(Comparador o) {
         // TODO Auto-generated method stub
